032186985cbbd97f8cbd0720050e35d29072f68d
Dmytrii <73241214+bdedwardovich@users.noreply.github.com>
2021-11-17 11:29:59 +0200

    Create caesar1.c

---
+++ caesar1.c
@@ -0,0 +1,41 @@
+/**
+ *  Encrypt
+ *
+    [pset2, задание #2]
+    1. Ввести ключ
+    2. Ввести текст который необходимо зашифровать
+    3. Зашифровать текст
+    4. Вывести на экран зашифрованное сообщение
+*
+**/
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+
+//1.Вводим ключ
+int main(int argc, string argv[])
+{
+    //проверка введен ли ключ
+    if (argc <= 1) {
+        printf("don't work, please, give the key.ヽ(‵﹏′)ノ.\n");
+        exit(0);
+    }
+    //проверка на валидность ключа (положительное ли это число)
+    int k = atoi(argv[1]);
+    if (k == 0 || k < 0)
+    {
+        printf("don't work, give the positive integer.ヽ(‵﹏′)ノ.\n");
+        exit(0);
+    }
+    //2.Вводим текст который необходимо зашифровать
+    string getstring = get_string("Please, give the text: ");
+    //3.Зашифровали текст
+    for (int i = 0, n = strlen(getstring); i < n; i++)
+    {
+        int letter = getstring[i];
+        int result = letter + k;
+        printf("Your text %c: %i | Encrypted %c: %i\n",letter, letter, result, result);
+    }
+    return printf("The work is done\n");
+}
